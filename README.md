1945037cdbe3be1574909ec8b3e70ce6a95c7017
mathewlee11 <mathewlee11@gmail.com>
2014-08-15 20:54:11 -0400

    s->left

---
+++ README.md
@@ -0,0 +1,5 @@
+Language Overview:
+
+Right now it's just a C program that takes in input and makes use of that,
+but once I know what the hell I'm doing that might change.
+
--- stacks.c
+++ stacks.c
@@ -6,6 +6,9 @@
 
 struct value {
 	int val;
+	int ret;
+	char type;
+	char * str;
 	struct value * next; 
 };
 
@@ -16,6 +19,8 @@ struct stack {
 	struct stack * right;
 };
 
+struct stack * loops;
+struct stack * funcs;
 char *readstr() {
    int cursize = 0;
    char * str = malloc(sizeof(char) * 4);
@@ -29,8 +34,7 @@ char *readstr() {
          maxsize = maxsize * 2;
           }
       *(str + cursize) = x;
-      cursize++; }
-     
+      cursize++; }  
    return str; }
 
 void val_push(int val, struct stack * s) {
@@ -51,11 +55,6 @@ int val_pop(struct stack * s) {
 	return val;  	          }
 
 
-int val_peek(struct stack * s) {
-	assert(s->top != NULL);
-	return s->top->val;
-	
-}
 struct stack *create_stack() {
 	struct stack *new = malloc(sizeof(struct stack));
 	new->top = NULL;
@@ -73,29 +72,70 @@ void destroy_stack(struct stack * s) {
 		free(current);
 		current = next; }
 	free(s); }
-	
-	
-int main() {
-	char * str = readstr();
 
-	int len = strlen(str);
-	int cur = 0;
-	char c;
-	char op = ' ';
+void loop_push(int val, int ret, char type, struct stack * s) {
+	struct value *new = malloc(sizeof(struct value));
+	new->val = val;
+	new->ret = ret;
+	new->type = type;
+	new->next = s->top;
+	s->top = new; }	
 	
-	struct stack * first = create_stack();
-	struct stack * s = first;
-	int i = 0;
-	struct stack * loops = create_stack();
-	for(i; i < len; i++) {
+void func_push(char name, char * str){
+	struct value * new = malloc(sizeof(struct value));
+	new->str = str;
+	new->val = name;
+	new->next = funcs->top;
+	funcs->top = new;
+}
+		
+
+char * get_func(char name, struct stack * s) {
+	struct value * current = s->top;
+	
+	while( current ) {
+		if (current->val == name) return current->str;
+		else current = current->next;
+	}
+	
+	return NULL;
+	
+}		 
+		 
+struct stack * process(char * str, struct stack * s, int cur){
+	int len = strlen(str);
+	char c;
+	char op = ' ';	 
+	int i;
+	for(i = 0; i < len; i++) {
 		c = *(str + i);
 		
 		if (c == 'v') {
 			val_push(cur,s);
 			cur = 0;
 		}
+		
+		else if (c == '`') {
+			val_push(cur,s);
+		}
+		
 	 
-		else if (c == '^') {
+		else if (c == '~') {
+			int num = s->top->val;
+			if (op == ' ') {
+				cur = num; 
+			}
+ 	
+			else {
+				if (op == '+') cur = cur + num;
+				else if (op == '-') cur = cur - num;
+				else if (op == '*') cur = cur * num;
+				else if (op == '/') cur = cur / num;
+				op = ' ';
+			}
+		}
+		
+		else if (c == '~') {
 			if (op == ' ') {
 				cur = val_pop(s); 
 			}
@@ -108,6 +148,7 @@ int main() {
 				op = ' ';
 			}
 		}
+			
 		
 		else if (c == '<') {
 			if (s ->left == NULL) {
@@ -142,43 +183,114 @@ int main() {
 			op = ' ';
 		}
 		
-		else if( c == 'p'){
+		else if( c == '#'){
 			printf("%d\n", cur);
 			}
-			
+		else if ( c == '@'){
+			printf("%c\n", cur);
+			}
+		
 		else if( c == '+' || c == '-' || c =='*' || c == '/') op = c;
 		
 		else if( c == '[') {
-			val_push(i, loops); 
+			loop_push(0, i, cur, loops); 
 		}
 		
 		else if ( c == ']') {
-			if (s->top == NULL) {
+		
+			if( loops->top->type != '[') {
+				printf("Error: attempted to close a '[' before closing the current '%c'.\n", loops->top->type);
+				exit(0);
+			}
+			else if (s->top == NULL) {
 				val_pop(loops); 
 			}
 			
 			else{
-				i = val_peek(loops);
+				i = loops->top->ret;
 			}
 		}
-		 		 
-	}	 		 
-	return 0; }
+		
+		else if (c == '{') {
+			
+			int cursize = 0;
+   			char * newstr = malloc(sizeof(char) * 4);
+   			int maxsize = 4;
+   			struct stack * newstack = create_stack();
+   			newstack->top = s->top;
+   			
+  		 	while(*(str + (++i)) != '}') {
+   
+      			if (cursize >= maxsize) {
+         			newstr = realloc(newstr, sizeof(char) * maxsize * 2);
+        			maxsize = maxsize * 2;
+         			}
+      			*(newstr + cursize) = *(str + i);
+      			cursize++;
+      			
+      		}
+      		
+      		s->top = (process(newstr, newstack, cur))->top;
+      		}
+      	
+		else if ( (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')){
+      			char * func = get_func(c,funcs);
+      		
+      			if ( *(str + i + 1) != '=' && (func == NULL)){
+      				printf("Error, expected '=' after %c. \n", c);
+      				exit(1);
+      			}
+      		
+      			else if ( *(str + i + 1) == '=' && func) {
+      				printf("Error, cannot redefine function %c. \n", c);
+      				exit(1);
+      			}
+      		
+      			else if (func) {
+      				process(func, s, cur);
+      			}
+      		
+      			else {
+      	
+      				int cursize = 0;
+   				char * newstr = malloc(sizeof(char) * 4);
+   				int maxsize = 4;
+   				struct stack * newstack = create_stack();
+   				newstack->top = s->top;
+   				char y = '{';
+   				y = c;
+  				i++;
+  				while(*(str + (++i)) != ';') {
+  	 
+      					if (cursize >= maxsize) {
+        	 				newstr = realloc(newstr, sizeof(char) * maxsize * 2);
+        					maxsize = maxsize * 2;
+         					}
+      					*(newstr + cursize) = *(str + i);
+      					cursize++;
+      			
+      					}
+      			func_push(c, newstr); 
+			}
+		}		
+	}
+	return s; 
+		
+}
+
 		 
 		 
 		 
 		 
 		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
-		 
+int main() {
+	char * str = readstr();
+	
+	struct stack * s = create_stack();
+	loops = create_stack();
+	funcs = create_stack();
+	process(str, s, 0);
+}		 
 		 
 		 
 		 
@@ -186,4 +298,18 @@ int main() {
 		 
 	
 	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
